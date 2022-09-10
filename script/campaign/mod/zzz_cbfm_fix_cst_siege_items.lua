ec061c106fc2922c3b83a4bbd84b37509c5962b5
vandy <chadvandewettering@gmail.com>
2022-09-09 20:42:33 -0400

    prevent script break on campaigns without VCoast

--- script/campaign/mod/zzz_cbfm_fix_cst_siege_items.lua
+++ script/campaign/mod/zzz_cbfm_fix_cst_siege_items.lua
@@ -4,6 +4,8 @@ local function cbfm_vampire_coast_tower_fix()
 	custom_eb:add_effect("CBFM_wh2_dlc11_effect_siege_engine_number_cst_siege_tower","faction_to_force_own",2)	
 
 	local harkon = cm:get_faction("wh2_dlc11_cst_vampire_coast")
+	if not harkon then return end
+
     cm:apply_custom_effect_bundle_to_faction(custom_eb,harkon)
 	
 	local all_coast = harkon:factions_of_same_subculture()
