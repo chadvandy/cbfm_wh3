26f4e74788d3fbc384befec369b52ddc3c259bce
Dux <mfvreeland@gmail.com>
2022-09-21 22:07:42 -0400

    Expanded functionality for all Chaos
    It turns out this problem was not only constrained to the Chaos DLC lords' military-focused techs. Instead, every Chaos tech that works via additional effects which are multiplied by active Chaos gifts does not persist when loading a game (or returning to campaign from a battle). This script and supporting DB content has been expanded to support redoing the functionality for all of these techs, allowing their states to be properly saved and loaded.

--- script/campaign/mod/zzz_cbfm_chs_mil_tech_init_fix.lua
+++ script/campaign/mod/zzz_cbfm_chs_mil_tech_init_fix.lua
@@ -1,60 +1,189 @@
-cbfm_chs_mil_tech_init_eb_table = {}
+cbfm_chs_mil_tech_init_eb_table = {["wh3_dlc20_chs_valkia"] = {},["wh3_dlc20_chs_azazel"] = {},["wh3_dlc20_chs_vilitch"] = {},["wh3_dlc20_chs_festus"] = {},["wh_main_chs_chaos"] = {},["wh3_dlc20_chs_sigvald"] = {},["wh3_dlc20_chs_kholek"] = {}}
 cbfm_chs_mil_tech_init_faction_info_table = 
 {
-	["wh3_dlc20_chs_valkia"] = {["tech"] = "wh3_dlc20_chs_kho_valkia_military_1",["init_set"] = "wh3_dlc20_faction_initiative_set_chs_khorne",["effect_bundle"] = "CBFM_upgrade_all_kho_weapon_strength",["base_value"] = 5},
-	["wh3_dlc20_chs_azazel"] = {["tech"] = "wh3_dlc20_chs_sla_azazel_military_1",["init_set"] = "wh3_dlc20_faction_initiative_set_chs_slaanesh",["effect_bundle"] = "CBFM_upgrade_all_sla_speed",["base_value"] = 5},
-	["wh3_dlc20_chs_vilitch"] = {["tech"] = "wh3_dlc20_chs_tze_vilitch_military_1",["init_set"] = "wh3_dlc20_faction_initiative_set_chs_tzeentch",["effect_bundle"] = "CBFM_upgrade_all_tze_physical_resistance",["base_value"] = 3},
-	["wh3_dlc20_chs_festus"] = {["tech"] = "wh3_dlc20_chs_nur_festus_military_1",["init_set"] = "wh3_dlc20_faction_initiative_set_chs_nurgle",["effect_bundle"] = "CBFM_upgrade_all_nur_healing_cap",["base_value"] = 10}
+	["wh3_dlc20_chs_valkia"] = 
+	{
+		["techs"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"wh3_dlc20_chs_kho_valkia_military_1","wh3_dlc20_chs_kho_shared_gift_upgrade_authority","wh3_dlc20_chs_kho_shared_gift_upgrade_corruption","wh3_dlc20_chs_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_corruption","wh3_dlc20_chs_und_shared_diplomacy"}
+		},
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"CBFM_upgrade_all_kho_weapon_strength","CBFM_upgrade_all_kho_authority","CBFM_upgrade_all_kho_corruption","CBFM_upgrade_all_kho_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_corruption","CBFM_upgrade_all_und_diplomacy"}
+		},
+		["base_value"] = 5
+	},
+	["wh3_dlc20_chs_azazel"] = 
+	{
+		["techs"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"wh3_dlc20_chs_sla_azazel_military_1","wh3_dlc20_chs_sla_shared_gift_upgrade_authority","wh3_dlc20_chs_sla_shared_gift_upgrade_corruption","wh3_dlc20_chs_sla_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_corruption","wh3_dlc20_chs_und_shared_diplomacy"}
+		},
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"CBFM_upgrade_all_sla_speed","CBFM_upgrade_all_sla_authority","CBFM_upgrade_all_sla_corruption","CBFM_upgrade_all_sla_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_corruption","CBFM_upgrade_all_und_diplomacy"}
+		},
+		["base_value"] = 5
+	},
+	["wh3_dlc20_chs_vilitch"] = 
+	{
+		["techs"] =
+		{
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"wh3_dlc20_chs_tze_vilitch_military_1","wh3_dlc20_chs_tze_shared_gift_upgrade_authority","wh3_dlc20_chs_tze_shared_gift_upgrade_corruption","wh3_dlc20_tze_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_corruption","wh3_dlc20_chs_und_shared_diplomacy"}
+		},
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"CBFM_upgrade_all_tze_physical_resistance","CBFM_upgrade_all_tze_authority","CBFM_upgrade_all_tze_corruption","CBFM_upgrade_all_tze_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_corruption","CBFM_upgrade_all_und_diplomacy"}
+		},
+		["base_value"] = 3
+	},
+	["wh3_dlc20_chs_festus"] = 
+	{
+		["techs"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"wh3_dlc20_chs_nur_festus_military_1","wh3_dlc20_chs_nur_shared_gift_upgrade_authority","wh3_dlc20_chs_nur_shared_gift_upgrade_corruption","wh3_dlc20_chs_nur_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_corruption","wh3_dlc20_chs_und_shared_diplomacy"}
+		},
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"CBFM_upgrade_all_nur_healing_cap","CBFM_upgrade_all_nur_authority","CBFM_upgrade_all_nur_corruption","CBFM_upgrade_all_nur_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_corruption","CBFM_upgrade_all_und_diplomacy"}
+		},
+		["base_value"] = 10
+	},
+	["wh_main_chs_chaos"] = 
+	{
+		["techs"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"wh3_dlc20_chs_kho_shared_gift_upgrade_authority","wh3_dlc20_chs_kho_shared_gift_upgrade_corruption","wh3_dlc20_chs_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"wh3_dlc20_chs_sla_shared_gift_upgrade_authority","wh3_dlc20_chs_sla_shared_gift_upgrade_corruption","wh3_dlc20_chs_sla_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"wh3_dlc20_chs_tze_shared_gift_upgrade_authority","wh3_dlc20_chs_tze_shared_gift_upgrade_corruption","wh3_dlc20_tze_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"wh3_dlc20_chs_nur_shared_gift_upgrade_authority","wh3_dlc20_chs_nur_shared_gift_upgrade_corruption","wh3_dlc20_chs_nur_shared_gift_upgrade_diplomacy"}
+		},		
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"CBFM_upgrade_all_kho_authority","CBFM_upgrade_all_kho_corruption","CBFM_upgrade_all_kho_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"CBFM_upgrade_all_sla_authority","CBFM_upgrade_all_sla_corruption","CBFM_upgrade_all_sla_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"CBFM_upgrade_all_tze_authority","CBFM_upgrade_all_tze_corruption","CBFM_upgrade_all_tze_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"CBFM_upgrade_all_nur_authority","CBFM_upgrade_all_nur_corruption","CBFM_upgrade_all_nur_diplomacy"}
+		},
+		["base_value"] = 1
+	},
+	["wh3_dlc20_chs_sigvald"] = 
+	{
+		["techs"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"wh3_dlc20_chs_kho_shared_gift_upgrade_authority","wh3_dlc20_chs_kho_shared_gift_upgrade_corruption","wh3_dlc20_chs_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"wh3_dlc20_chs_sla_shared_gift_upgrade_authority","wh3_dlc20_chs_sla_shared_gift_upgrade_corruption","wh3_dlc20_chs_sla_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"wh3_dlc20_chs_tze_shared_gift_upgrade_authority","wh3_dlc20_chs_tze_shared_gift_upgrade_corruption","wh3_dlc20_tze_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"wh3_dlc20_chs_nur_shared_gift_upgrade_authority","wh3_dlc20_chs_nur_shared_gift_upgrade_corruption","wh3_dlc20_chs_nur_shared_gift_upgrade_diplomacy"}
+		},		
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"CBFM_upgrade_all_kho_authority","CBFM_upgrade_all_kho_corruption","CBFM_upgrade_all_kho_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"CBFM_upgrade_all_sla_authority","CBFM_upgrade_all_sla_corruption","CBFM_upgrade_all_sla_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"CBFM_upgrade_all_tze_authority","CBFM_upgrade_all_tze_corruption","CBFM_upgrade_all_tze_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"CBFM_upgrade_all_nur_authority","CBFM_upgrade_all_nur_corruption","CBFM_upgrade_all_nur_diplomacy"}
+		},
+		["base_value"] = 1
+	},
+	["wh3_dlc20_chs_kholek"] = 
+	{
+		["techs"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"wh3_dlc20_chs_und_shared_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"wh3_dlc20_chs_kho_shared_gift_upgrade_authority","wh3_dlc20_chs_kho_shared_gift_upgrade_corruption","wh3_dlc20_chs_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"wh3_dlc20_chs_sla_shared_gift_upgrade_authority","wh3_dlc20_chs_sla_shared_gift_upgrade_corruption","wh3_dlc20_chs_sla_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"wh3_dlc20_chs_tze_shared_gift_upgrade_authority","wh3_dlc20_chs_tze_shared_gift_upgrade_corruption","wh3_dlc20_tze_kho_shared_gift_upgrade_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"wh3_dlc20_chs_nur_shared_gift_upgrade_authority","wh3_dlc20_chs_nur_shared_gift_upgrade_corruption","wh3_dlc20_chs_nur_shared_gift_upgrade_diplomacy"}
+		},		
+		["effect_bundles"] = 
+		{
+			["wh3_dlc20_faction_initiative_set_chs_undivided"] = {"CBFM_upgrade_all_und_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_khorne"] = {"CBFM_upgrade_all_kho_authority","CBFM_upgrade_all_kho_corruption","CBFM_upgrade_all_kho_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_slaanesh"] = {"CBFM_upgrade_all_sla_authority","CBFM_upgrade_all_sla_corruption","CBFM_upgrade_all_sla_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_tzeentch"] = {"CBFM_upgrade_all_tze_authority","CBFM_upgrade_all_tze_corruption","CBFM_upgrade_all_tze_diplomacy"},
+			["wh3_dlc20_faction_initiative_set_chs_nurgle"] = {"CBFM_upgrade_all_nur_authority","CBFM_upgrade_all_nur_corruption","CBFM_upgrade_all_nur_diplomacy"}
+		},
+		["base_value"] = 1
+	}
 }
 
 function cbfm_chs_mil_tech_init_update(faction)
 
-	--ModLog("DUX: cbfm_chs_mil_tech_init_update function started with parameter " .. tostring(faction))
+	ModLog("DUX: cbfm_chs_mil_tech_init_update function started with parameter " .. tostring(faction))
 	
 	local faction_entry = cbfm_chs_mil_tech_init_faction_info_table[faction]
 	
-	-- if an invalid faction was passed or we don't have the right tech, we can stop right here
-	if not faction_entry then
-		--ModLog("DUX: invalid parameter or non-applicable faction, exiting")
+	-- if an invalid faction was passed, or faction doesn't exist (e.g., realms campaign), or faction is dead: we can stop right here
+	if not faction_entry or not cm:get_faction(faction) or cm:get_faction(faction):is_dead() then
+		ModLog("DUX: invalid parameter or non-applicable faction, exiting")
 		return nil 
-	end
-	if not cm:get_faction(faction):has_technology(faction_entry.tech) then
-		--ModLog("DUX: required tech not unlocked yet, exiting")
-		return nil
-	end
-	
-	-- create our custom effect bundle interface if it doesn't already exist
-	if not cbfm_chs_mil_tech_init_eb_table[faction] then 
-		--ModLog("DUX: custom effect bundle not yet created for " .. faction .. ", creating now")
-		cbfm_chs_mil_tech_init_eb_table[faction] = cm:create_new_custom_effect_bundle(faction_entry.effect_bundle)
-	else
-		--ModLog("DUX: custom effect bundle already created for " .. faction .. ", using existing bundle")
 	end
 	
 	-- call upon the forces of CCO to get active faction initiative data
-	local num_initiative_sets = common.get_context_value("CcoCampaignFaction",faction,"InitiativeSetList.Size")
-	local num_active = 0
+	local active_inits = {}
+	local num_initiative_sets = common.get_context_value("CcoCampaignFaction",faction,"InitiativeSetList.Size")  -- defining this here just in case something changes in the future that would cause the initiative set list for a faction to differ from what we assume in our info table above
 	
-	for i = 0, (num_initiative_sets - 1) do
-		local set_to_check = common.get_context_value("CcoCampaignFaction",faction,("InitiativeSetList[" .. tostring(i) .. "].InitiativeSetContext.Key"))
-		if set_to_check == faction_entry.init_set then
-			num_active = common.get_context_value("CcoCampaignFaction",faction,("InitiativeSetList[" .. tostring(i) .. "].ActiveInitiatives.Size"))
-			--ModLog("DUX: valid initiative set found with " .. tostring(num_active) .. " initiatives active")
+	for init_set, techs_per_set in pairs(faction_entry.techs) do
+	
+		for i = 0, (num_initiative_sets - 1) do
+			ModLog(tostring(num_initiative_sets) .. "|" .. tostring(i))
+			local set_to_check = common.get_context_value("CcoCampaignFaction",faction,("InitiativeSetList[" .. tostring(i) .. "].InitiativeSetContext.Key"))
+			if set_to_check == init_set then
+				active_inits[init_set] = common.get_context_value("CcoCampaignFaction",faction,("InitiativeSetList[" .. tostring(i) .. "].ActiveInitiatives.Size"))
+				ModLog("DUX: valid initiative set " .. init_set .. " found with " .. tostring(active_inits[init_set]) .. " initiatives active")
+			break end
+		end
+		
+		for index, tech in ipairs(techs_per_set) do		
+			-- check if we have this tech; proceed only if we do
+			if not cm:get_faction(faction):has_technology(tech) then
+				ModLog("DUX: tech " .. tech .. " not unlocked for " .. faction .. " yet, skipping")
+			else
+				ModLog("DUX: Tech " .. tech .. " attained, proceeding with effects")
+				-- create our custom effect bundle interface if it doesn't already exist
+				if not cbfm_chs_mil_tech_init_eb_table[faction][tech] then 
+					ModLog("DUX: custom effect bundle not yet created for " .. faction .. ":" .. tech ..  ", creating now")
+					cbfm_chs_mil_tech_init_eb_table[faction][tech] = cm:create_new_custom_effect_bundle(faction_entry.effect_bundles[init_set][index])
+				else
+					ModLog("DUX: custom effect bundle already created for " .. faction .. ":" .. tech .. ", using existing bundle")
+				end
+				
+				if active_inits[init_set] == 0 then ModLog("DUX: no active initatives were found, setting effect value for " .. faction .. ":" .. tech .. " to zero") end
+				
+				-- determine the correct base value for this effect using the power of deductive reasoning
+				local base_value = faction_entry.base_value -- first, assume we are using the custom base value for champion LLs
+				if init_set == "wh3_dlc20_faction_initiative_set_chs_undivided" then base_value = 2 end  -- next, check if this is the undivided set. if it is, presume we need the base value for undivided corruption, which is 2
+				if index > 1 then base_value = 1 end -- if index is > 1, that means we're looking at a tech that cannot be champion unique (custom base value) or undivided corruption (base value = 2); assume base value is 1
+				if index == #techs_per_set then base_value = 15 end -- finally, diplomacy techs are at the end of every table, so if we're looking at the last one, we know it needs a base value of 15
+				
+				-- modify our effect bundle as required
+				local num_effects = cbfm_chs_mil_tech_init_eb_table[faction][tech]:effects():num_items()
+				for i = 0, (num_effects - 1) do
+					local effect = cbfm_chs_mil_tech_init_eb_table[faction][tech]:effects():item_at(i)
+					cbfm_chs_mil_tech_init_eb_table[faction][tech]:set_effect_value(effect,(base_value * active_inits[init_set])) -- we can only get away with using the same math for all effects in a bundle because the only bundle with multiple effects is und_diplomacy, and each effect in that bundle requires the same value (15)
+				end
+				-- apply our effect bundle
+				ModLog("DUX: custom effect bundle now being applied for " .. faction .. ":" .. tech .. " with base value: " .. tostring(base_value) .. " and multiplier: " .. tostring(active_inits[init_set]))
+				cm:apply_custom_effect_bundle_to_faction(cbfm_chs_mil_tech_init_eb_table[faction][tech],cm:get_faction(faction))
+			end
 		end
 	end
-	
-	--if num_active == 0 then ModLog("DUX: no active initatives were found, setting effect value for " .. faction .. " to zero") end
-	
-	-- modify our effect bundle as required
-	local effect = cbfm_chs_mil_tech_init_eb_table[faction]:effects():item_at(0) -- there is only one effect in each of these bundles
-	cbfm_chs_mil_tech_init_eb_table[faction]:set_effect_value(effect,(faction_entry.base_value * num_active))
-	
-	-- apply our effect bundle
-	--ModLog("DUX: custom effect bundle now being applied for " .. faction)
-	cm:apply_custom_effect_bundle_to_faction(cbfm_chs_mil_tech_init_eb_table[faction],(cm:get_faction(faction)))
 end
 
 for faction, v in pairs(cbfm_chs_mil_tech_init_faction_info_table) do
 	cm:add_first_tick_callback(function() cbfm_chs_mil_tech_init_update(faction) end)
 	cm:add_faction_turn_start_listener_by_name("cbfm_chs_mil_tech_init_listener",faction,function() cbfm_chs_mil_tech_init_update(faction) end,true)
+	--core:add_listener("cbfm_chs_mil_tech_init_listener","FactionTurnEnd",function(context) return context:faction():name() == faction end,function() cbfm_chs_mil_tech_init_update(faction) end,true)
 end
