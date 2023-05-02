d61e6f3e7e2a8c375be0ec8b4e139da23c8014b1
Dux <mfvreeland@gmail.com>
2023-05-02 12:11:54 -0400

    Fix for event fix post-battle unreliability
    We're just gonna check for two seconds now. In my testing, this seems to cover everything.

--- script/campaign/mod/zzz_cbfm_event_feed_fix.lua
+++ script/campaign/mod/zzz_cbfm_event_feed_fix.lua
@@ -17,29 +17,31 @@ end
 
 function dux_event_feed_fix_first_loop()
 	local hud_uic = find_uicomponent("hud_campaign")
-	if hud_uic and hud_uic:Visible() then
-		ModLog("DUX_EVENT_FEED_FIX: hud_uic found, waiting one game model second")
-		cm:callback(dux_event_feed_fix_persistent_hud,1)
-		cm:remove_real_callback("dux_event_feed_fix_first_loop")
-	elseif not cm:is_human_factions_turn() then
+	if not cm:is_human_factions_turn() then
 		ModLog("DUX_EVENT_FEED_FIX: events check ended/skipped because this is not (or no longer) a player turn")
+		cm:remove_real_callback("dux_event_feed_fix_first_loop")
+		return nil
+	elseif hud_uic and hud_uic:Visible() then
+		ModLog("DUX_EVENT_FEED_FIX: hud_uic found, starting event feed loop")
+		cm:repeat_real_callback(dux_event_feed_fix_second_loop,250,"dux_event_feed_fix_second_loop")
 		cm:remove_real_callback("dux_event_feed_fix_first_loop")
 	end
 end
 
-function dux_event_feed_fix_persistent_hud()
-	local hud_uic = find_uicomponent("hud_campaign")
-	if hud_uic and hud_uic:Visible() then
-		ModLog("DUX_EVENT_FEED_FIX: hud_uic is still visible after one game model second, beginning events loop")
-		cm:repeat_real_callback(dux_event_feed_fix_second_loop,250,"dux_event_feed_fix_second_loop")
-	else
-		ModLog("DUX_EVENT_FEED_FIX: hud_uic is no longer visible after one game model second, could be at post-battle screen or in a cutscene, waiting for hud_uic to come back before proceeding")
-		cm:repeat_real_callback(dux_event_feed_fix_first_loop,250,"dux_event_feed_fix_first_loop")
-	end
-end
+--function dux_event_feed_fix_persistent_hud()
+--	local hud_uic = find_uicomponent("hud_campaign")
+--	if hud_uic and hud_uic:Visible() then
+--		ModLog("DUX_EVENT_FEED_FIX: hud_uic is still visible after one game model second, beginning events loop")
+--		cm:repeat_real_callback(dux_event_feed_fix_second_loop,250,"dux_event_feed_fix_second_loop")
+--	else
+--		ModLog("DUX_EVENT_FEED_FIX: hud_uic is no longer visible after one game model second, could be at post-battle screen or in a cutscene, waiting for hud_uic to come back before proceeding")
+--		cm:repeat_real_callback(dux_event_feed_fix_first_loop,250,"dux_event_feed_fix_first_loop")
+--	end
+--end
 
 function dux_event_feed_fix_second_loop()
 	local events_uic = find_uicomponent("events")
+	local hud_uic = find_uicomponent("hud_campaign")
 	if not events_uic then
 		ModLog("DUX_EVENT_FEED_FIX: events_uic not found, attempting to stir up")
 		cm:suppress_all_event_feed_event_types(false)
@@ -47,13 +49,20 @@ function dux_event_feed_fix_second_loop()
 	else
 		dux_event_feed_fix_counter = 0
 	end
-	if dux_event_feed_fix_counter >= 4 then
-		ModLog("DUX_EVENT_FEED_FIX: events_uic has not been present for four cycles (one full second), priority events seem to be done, no longer checking")
+	if dux_event_feed_fix_counter >= 8 then
+		ModLog("DUX_EVENT_FEED_FIX: events_uic has not been present for eight cycles (two full seconds), priority events seem to be done, no longer checking")
 		cm:remove_real_callback("dux_event_feed_fix_second_loop")
 	end
 	if not cm:is_human_factions_turn() then
 		ModLog("DUX_EVENT_FEED_FIX: events check ended/skipped because this is not (or no longer) a player turn")
 		cm:remove_real_callback("dux_event_feed_fix_second_loop")
+		return nil
+	end
+	if not hud_uic:Visible() then
+		ModLog("DUX_EVENT_FEED_FIX: hud_uic lost visibility while checking for priority events, but this is still a player turn; could be post-battle or a cutscene, restarting hud check loop")
+		dux_event_feed_fix_counter = 0
+		cm:remove_real_callback("dux_event_feed_fix_second_loop")
+		cm:repeat_real_callback(dux_event_feed_fix_first_loop,250,"dux_event_feed_fix_first_loop")
 	end
 end
 
