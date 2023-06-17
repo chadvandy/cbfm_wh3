20c98ed1969169f34062d419b7d16896b886a7b0
Jianni <kingpappo@protonmail.com>
2023-06-17 03:15:25 +0200

    #520 #553

---
+++ script/campaign/mod/zzz_cbfm_agemouk_ai_caskets.lua
@@ -0,0 +1,29 @@
+local tomb_kings_culture = "wh2_dlc09_tmb_tomb_kings"
+
+local function ai_caskets()
+	-- Automatically on defined turn unlocks caskets for AI from the crafted RoR's
+	core:add_listener(
+		"turn_start_ai_workshop_crafted_AI",
+		"FactionTurnStart",
+		function(context)
+            local faction = context:faction()
+			return faction:culture() == tomb_kings_culture and faction:is_human() == false;
+		end,
+		function(context)
+			local turn = cm:model():turn_number();
+            local faction = context:faction()
+            local faction_key = faction:name()
+			if turn > 1 then 
+			cm:remove_event_restricted_unit_record_for_faction("wh2_dlc09_tmb_art_casket_of_souls_0", faction_key);
+        else
+				out("Nothing more to unlock")
+        end
+        end,
+		true
+	);
+
+end
+
+cm:add_first_tick_callback(function()
+	cm:callback(ai_caskets, 2.5)
+end);
