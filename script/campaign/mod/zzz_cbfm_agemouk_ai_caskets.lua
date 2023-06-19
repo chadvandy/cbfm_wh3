ea05f4cb22d9b00d771a1b2b48611502271a5f1a
Jianni <kingpappo@protonmail.com>
2023-06-19 20:24:08 +0200

    #520 using Dux script instead of Agemouk

--- script/campaign/mod/zzz_cbfm_agemouk_ai_caskets.lua
+++ script/campaign/mod/zzz_cbfm_agemouk_ai_caskets.lua
@@ -1,29 +1,17 @@
-local tomb_kings_culture = "wh2_dlc09_tmb_tomb_kings"
-
-local function zzz_cbfm_agemouk_ai_caskets()
-	-- Automatically on defined turn unlocks caskets for AI from the crafted RoR's
-	core:add_listener(
-		"turn_start_ai_workshop_crafted_AI",
-		"FactionTurnStart",
-		function(context)
-            local faction = context:faction()
-			return faction:culture() == tomb_kings_culture and faction:is_human() == false;
-		end,
-		function(context)
-			local turn = cm:model():turn_number();
-            local faction = context:faction()
-            local faction_key = faction:name()
-			if turn > 1 then 
-			cm:remove_event_restricted_unit_record_for_faction("wh2_dlc09_tmb_art_casket_of_souls_0", faction_key);
-        else
-				out("Nothing more to unlock")
+-- script actually by Dux
+core:add_listener(
+    "tomb_king_ror_unlock_ai",
+    "RitualCompletedEvent",
+    function(context)
+        return not context:performing_faction():is_human()
+    end,
+    function(context)
+        local faction_key = context:performing_faction():name()
+        local ritual_key = context:ritual():ritual_key()
+        
+        if ritual_key == "wh2_dlc09_ritual_tmb_tahoth" then
+            cm:remove_event_restricted_unit_record_for_faction("wh2_dlc09_tmb_art_casket_of_souls_0", faction_key)
         end
-        end,
-		true
-	);
-
-end
-
-cm:add_first_tick_callback(function()
-	cm:callback(zzz_cbfm_agemouk_ai_caskets, 2.5)
-end);
+    end,
+    true
+)
