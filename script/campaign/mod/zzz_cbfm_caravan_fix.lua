fce3ae762f244121c2fee41df046c557b11339fc
Dux <mfvreeland@gmail.com>
2023-11-21 14:29:59 -0500

    #915 partially fixed by CA
    "Clandestine" fixed in 4.1

--- script/campaign/mod/zzz_cbfm_caravan_fix.lua
+++ script/campaign/mod/zzz_cbfm_caravan_fix.lua
@@ -7,22 +7,3 @@ caravans.traits_to_units.wh3_dlc23_skill_innate_chd_convoy_overseer_retired_daem
 		"wh3_dlc23_chd_inf_chaos_dwarf_blunderbusses",
 		"wh3_dlc23_chd_cav_bull_centaurs_greatweapons"
 		}
-	
-	
-local function zzz_fix_caravan_master_stealth()
-	core:add_listener(
-		"zzz_fix_caravan_master_stealth",
-		"CaravanRecruited",
-		function(context)
-			return context:caravan_master():character():background_skill() == "wh3_main_skill_innate_cth_caravan_master_stealth"
-		end,
-		function(context)
-			local lord_cqi = context:caravan_master():character():command_queue_index()
-			local lord_str = cm:char_lookup_str(lord_cqi)
-			cm:add_experience_to_units_commanded_by_character(lord_str, 3)
-		end,
-		true
-	)
-end
-
-cm:add_first_tick_callback(zzz_fix_caravan_master_stealth)
