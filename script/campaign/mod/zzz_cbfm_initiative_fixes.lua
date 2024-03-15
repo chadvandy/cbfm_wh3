db4c632155693519120077f9c3ad9f8e77216671
Dux <mfvreeland@gmail.com>
2024-03-15 12:39:41 -0400

    Fix for #581
    Adding to the new initiative fixes script

--- script/campaign/mod/zzz_cbfm_initiative_fixes.lua
+++ script/campaign/mod/zzz_cbfm_initiative_fixes.lua
@@ -61,6 +61,23 @@ function cbfm_update_initiatives()
 				["grant_immediately"] = true
 			}
 		end
+		-- "Wastelander" fix
+		if value.initiative_key[1] == "wh3_dlc20_character_initiative_chs_exalted_hero_nur_06" then
+			initiative_templates[key] =
+			{
+				["initiative_key"] = 
+					{
+						"wh3_dlc20_character_initiative_chs_exalted_hero_nur_06", "wh3_dlc20_character_initiative_chs_chaos_sorcerer_lord_nur_04", -- Nurgle - Corruption
+					},
+				["event"] = "GarrisonOccupiedEvent",
+				["condition"] =
+					function(context)
+						return context:garrison_residence():region():settlement():get_climate() == "climate_chaotic";
+					end,
+				-- CBFM: this was missing from the original script
+				["grant_immediately"] = true 
+			}
+		end
 	end
 end
 
