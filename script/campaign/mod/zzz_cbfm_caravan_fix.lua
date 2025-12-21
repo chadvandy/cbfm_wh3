253c874be79ce7e79b8e2d5ac92f1fec8b001dd0
Dux <mfvreeland@gmail.com>
2025-12-21 16:43:01 -0500

    #915 fixed by CA
    Fully fixed at some point

--- script/campaign/mod/zzz_cbfm_caravan_fix.lua
+++
@@ -1,9 +0,0 @@
-caravans.traits_to_units.wh3_dlc23_skill_innate_chd_convoy_overseer_retired_daemonsmither = {
-		"wh3_dlc23_chd_cha_bull_centaur_taurruk",
-		"wh3_dlc23_chd_inf_chaos_dwarf_warriors",
-		"wh3_dlc23_chd_inf_chaos_dwarf_warriors",
-		"wh3_dlc23_chd_inf_chaos_dwarf_blunderbusses",
-		"wh3_dlc23_chd_inf_chaos_dwarf_blunderbusses",
-		"wh3_dlc23_chd_inf_chaos_dwarf_blunderbusses",
-		"wh3_dlc23_chd_cav_bull_centaurs_greatweapons"
-		}
