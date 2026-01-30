75117b89e0ae9ccc25b45961a9433bba715a2573
Dux <mfvreeland@gmail.com>
2026-01-30 13:10:53 -0500

    Fix for #2498. Styrkaar for Azazel
    Adds Azazel's faction to the list of valid factions for Styrkaar spawning

---
+++ script/campaign/mod/zzz_cbfm_legendary_character_script_fixes.lua
@@ -0,0 +1,12 @@
+-- Adding Styrkaar for Azazel
+character_unlocking.character_data.styrkaar.override_allowed_factions = 
+{
+	"wh3_dlc20_chs_kholek",
+	"wh_main_chs_chaos",
+	"wh3_main_chs_shadow_legion",
+	"wh3_dlc20_chs_sigvald",
+	"wh3_dlc27_sla_masque_of_slaanesh",
+	"wh3_dlc27_sla_the_tormentors",
+	"wh3_main_sla_seducers_of_slaanesh",
+	"wh3_dlc20_chs_azazel" -- CBFM: Adding Azazel to the list of valid factions for Styrkaar
+}
