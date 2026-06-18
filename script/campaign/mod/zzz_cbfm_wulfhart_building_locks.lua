76d61346900a0a3212527ee4c2db5a0726b3ee3d
Dux <mfvreeland@gmail.com>
2026-06-18 18:13:22 -0400

    Update zzz_cbfm_wulfhart_building_locks.lua
    I wasn't properly account for initial locks before. This should cover all bases.

--- script/campaign/mod/zzz_cbfm_wulfhart_building_locks.lua
+++ script/campaign/mod/zzz_cbfm_wulfhart_building_locks.lua
@@ -11,11 +11,17 @@ function update_acclaim_bar(factor)
 	
 	-- BEGIN CBFM FIX
 	local function lock_wulfhart_buildings(index)
-		for i = 1, #wulfhart_buildings_to_unlock[index] do -- Abandoning the pointless "wulfhart_buildings_to_lock" array, which was getting mismatched, in favor of the same table used for unlocking, "wulfhart_buildings_to_unlock"
-			cm:add_event_restricted_building_record_for_faction(wulfhart_buildings_to_unlock[index][i], wulfhart_faction, "wulfhart_building_lock")
+		-- index == 0 is only used to set up initial locks for Wulfhart
+		if index == 0 then
+			for i = 1, #wulfhart_buildings_to_lock do
+				cm:add_event_restricted_building_record_for_faction(wulfhart_buildings_to_lock[i], wulfhart_faction, "wulfhart_building_lock")
+			end
+		else
+			for i = 1, #wulfhart_buildings_to_unlock[index] do -- Instead of using "wulfhart_buildings_to_lock" array here, which was getting mismatched, we are reusing ehe same table used for unlocking, "wulfhart_buildings_to_unlock"
+				cm:add_event_restricted_building_record_for_faction(wulfhart_buildings_to_unlock[index][i], wulfhart_faction, "wulfhart_building_lock")
+			end
 		end
 	end
-	-- END CBFM FIX
 	
 	-- trigger an event reminding about progression reward
 	for i = 1, #acclaim_thresholds do
@@ -27,10 +33,11 @@ function update_acclaim_bar(factor)
 			end
 		elseif previous_acclaim_value >= acclaim_thresholds[i] and current_acclaim_value < acclaim_thresholds[i] then
 			lock_wulfhart_buildings(i + 1)
-		elseif current_acclaim_value < acclaim_thresholds[1] then -- CBFM note: This last elseif is actually 100% pointless, but it won't break anything, so we'll leave it in for posterity
-			lock_wulfhart_buildings(1)
+		elseif current_acclaim_value < acclaim_thresholds[1] then -- CBFM note: This is only needed to set up the initial locks
+			lock_wulfhart_buildings(0) -- CBFM note: Changing this to 0 to indicate to our modified function that we just want to lock everything to get to the initial state
 		end
 	end
+	-- END CBFM FIX
 	
 	update_imperial_reinforcements_strength()
 	
